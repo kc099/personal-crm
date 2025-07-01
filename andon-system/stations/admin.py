5f7271bd47e4c1dfa9d1e840c95a84d84e2e31e3
lohithmahamkali29 <lohithmahankali123@gmail.com>
2025-07-01 18:06:06 +0530

    added models

--- andon-system/stations/admin.py
+++ andon-system/stations/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib import admin
+from .models import BayDetail, DailyRecord, SectionData, ShiftData, ShiftConfig, ShiftBaselines
+
+admin.site.register(BayDetail)
+admin.site.register(DailyRecord)
+admin.site.register(SectionData)
+admin.site.register(ShiftData)
+admin.site.register(ShiftConfig)
+admin.site.register(ShiftBaselines)
 
 # Register your models here.
--- andon-system/stations/models.py
+++ andon-system/stations/models.py
@@ -1,3 +1,94 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class BayDetail(models.Model):
+    StationName = models.CharField(max_length=50, unique=True)
+    PlannedCount1 = models.IntegerField(default=0)
+    PlannedCount2 = models.IntegerField(default=0)
+    PlannedCount3 = models.IntegerField(default=0)
+    ActualCount = models.IntegerField(default=0)
+    Efficiency = models.FloatField(default=0.00)
+    ipAddress = models.CharField(max_length=100, null=True, blank=True)
+    Topic = models.CharField(max_length=100, null=True, blank=True)
+    isactive = models.BooleanField(default=True)
+    isalive = models.BooleanField(default=True)
+    DateCreated = models.DateTimeField(default=timezone.now)
+    totalDowntime = models.FloatField(default=0.00)
+    calltype_index_map = models.TextField(
+        default='{"PMD":0,"Quality":2,"Store":6,"JMD":8,"Production":12}'
+    )
+
+    def __str__(self):
+        return self.StationName
+
+
+class DailyRecord(models.Model):
+    id = models.AutoField(primary_key=True)
+    StationName = models.ForeignKey(BayDetail, to_field="StationName", on_delete=models.CASCADE)
+    TodayDate = models.DateField()
+    Plan = models.IntegerField(default=0)
+    ActualCount = models.IntegerField(default=0)
+    Efficiency = models.FloatField(default=0.0)
+    mDowntime = models.FloatField(default=0.0)
+    pDowntime = models.FloatField(default=0.0)
+    qDowntime = models.FloatField(default=0.0)
+    sDowntime = models.FloatField(default=0.0)
+    jDowntime = models.FloatField(default=0.0)
+    totalDowntime = models.FloatField(default=0.0)
+    shift = models.IntegerField(null=True)
+
+    def __str__(self):
+        return f"{self.StationName} - {self.TodayDate} - Shift {self.shift}"
+
+
+class SectionData(models.Model):
+    id = models.AutoField(primary_key=True)
+    StationName = models.ForeignKey(BayDetail, to_field="StationName", on_delete=models.CASCADE)
+    calltype = models.CharField(max_length=20)
+    FaultTime = models.DateTimeField(null=True, blank=True)
+    ResolvedTime = models.DateTimeField(null=True, blank=True)
+    DateTime = models.DateTimeField(null=True, blank=True)
+    Shift = models.IntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.StationName} - {self.calltype}"
+
+
+class ShiftData(models.Model):
+    StationName = models.OneToOneField(BayDetail, to_field="StationName", on_delete=models.CASCADE, primary_key=True)
+    last_actual_count = models.IntegerField(default=0)
+    shift1_actual = models.IntegerField(default=0)
+    shift2_actual = models.IntegerField(default=0)
+    shift3_actual = models.IntegerField(default=0)
+    Date = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.StationName} - {self.Date}"
+
+
+class ShiftConfig(models.Model):
+    id = models.AutoField(primary_key=True)
+    shift1_start = models.TimeField(default="05:30")
+    shift1_end = models.TimeField(default="14:20")
+    shift2_start = models.TimeField(default="14:20")
+    shift2_end = models.TimeField(default="00:10")
+    shift3_start = models.TimeField(default="00:10")
+    shift3_end = models.TimeField(default="05:30")
+
+    def __str__(self):
+        return "ShiftConfig"
+
+
+class ShiftBaselines(models.Model):
+    id = models.AutoField(primary_key=True)
+    StationName = models.ForeignKey(BayDetail, to_field="StationName", on_delete=models.CASCADE)
+    Shift = models.IntegerField()
+    Date = models.DateField()
+    BaselineCount = models.IntegerField(default=0)
+    CreatedAt = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('StationName', 'Shift', 'Date')
+
+    def __str__(self):
+        return f"{self.StationName} - Shift {self.Shift} - {self.Date}"
